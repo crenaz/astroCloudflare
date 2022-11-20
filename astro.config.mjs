23037176573e603f725a3aaf6c913ff526e12526
crenaz <crenaz@gmail.com>
2022-11-20 18:41:53 -0400

    641

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,12 @@ import mdx from '@astrojs/mdx';
 //import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
+import cloudflare from "@astrojs/cloudflare";
+
+// https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx()],
+  site: 'https://example.com',
+  integrations: [mdx()],
+  output: "server",
+  adapter: cloudflare()
 });
