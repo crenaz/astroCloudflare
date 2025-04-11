636f0c29e690d50c0dcf964f0998a8d5a0d69688
crenaz <crenaz@gmail.com>
2025-04-11 13:15:33 -0400

    older astro now working with cloudflare?

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import cloudflare from "@astrojs/cloudflare";
+import react from "@astrojs/react";
 
 export default defineConfig({
   site: "https://example.com",
-  integrations: [mdx()],
+  integrations: [mdx(), react()],
   output: "server",
   adapter: cloudflare({
     imageService: "passthrough",
